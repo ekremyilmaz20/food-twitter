36448f1c7c990fef20f3437ed353d35309cdd3d3
ekremyilmaz20 <114732823+ekremyilmaz20@users.noreply.github.com>
2024-04-02 17:30:25 +0300

    Push-7

--- foodTwitter/test_app.py
+++ foodTwitter/test_app.py
@@ -1,5 +1,5 @@
 import unittest
-from app import app, db
+from app import app, db, Recipe
 
 
 class TestApp(unittest.TestCase):
@@ -30,6 +30,68 @@ class TestApp(unittest.TestCase):
     def test_get_all_recipes(self):
         response = self.app.get('/recipes')
         self.assertEqual(response.status_code, 200)
+
+    def test_delete_recipe(self):
+        # Create a recipe to be deleted
+        new_recipe = Recipe(
+            recipe_name='Test Recipe',
+            ingredients='Ingredient 1, Ingredient 2',
+            instructions='Test instructions',
+            cooking_time=30,
+        )
+        db.session.add(new_recipe)
+        db.session.commit()
+
+        # Get the ID of the created recipe
+        recipe_id = new_recipe.recipe_id
+
+        # Send a POST request to delete the recipe
+        response = self.app.post(f'/recipes/{recipe_id}/delete')
+
+        # Check if the response redirects to the recipes page after deletion
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('/all_recipes', response.headers['Location'])
+
+        # Check if the recipe is deleted from the database
+        deleted_recipe = db.session.query(Recipe).get(recipe_id)
+        self.assertIsNone(deleted_recipe)
+
+    def test_update_recipe(self):
+        # Create a recipe to be updated
+        new_recipe = Recipe(
+            recipe_name='Test Recipe',
+            ingredients='Ingredient 1, Ingredient 2',
+            instructions='Test instructions',
+            cooking_time=30,
+        )
+        db.session.add(new_recipe)
+        db.session.commit()
+
+        # Get the ID of the created recipe
+        recipe_id = new_recipe.recipe_id
+
+        # Data for updating the recipe
+        updated_data = {
+            'recipe_name': 'Updated Test Recipe',
+            'ingredients': 'Updated Ingredient 1, Updated Ingredient 2',
+            'instructions': 'Updated instructions',
+            'cooking_time': 45
+        }
+
+        # Send a POST request to update the recipe
+        response = self.app.post(f'/recipes/{recipe_id}', data=updated_data)
+
+        # Check if the response redirects to the recipes page after update
+        self.assertEqual(response.status_code, 302)
+        self.assertIn('/all_recipes', response.headers['Location'])
+
+        # Check if the recipe is updated in the database
+        updated_recipe = db.session.query(Recipe).get(recipe_id)
+        self.assertIsNotNone(updated_recipe)
+        self.assertEqual(updated_recipe.recipe_name, updated_data['recipe_name'])
+        self.assertEqual(updated_recipe.ingredients, updated_data['ingredients'])
+        self.assertEqual(updated_recipe.instructions, updated_data['instructions'])
+        self.assertEqual(updated_recipe.cooking_time, updated_data['cooking_time'])
 
 
 if __name__ == '__main__':
